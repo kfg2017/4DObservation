1fc9d358a0dba6342c17bfa063be45f5049cbd2c
krju1012 <julian.krauth@eifer.uni-karlsruhe.de>
2017-12-12 11:41:52 +0100

    Point cloud  Xampp Integration + Visualisation

--- app/src/main/java/kfg_geoinfomatics/a4dobservation/Pointcloud.java
+++ app/src/main/java/kfg_geoinfomatics/a4dobservation/Pointcloud.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 /**
@@ -18,12 +21,24 @@ public class Pointcloud extends Fragment {
         // Required empty public constructor
     }
 
+    public WebView mWebview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pointcloud, container, false);
+        View v = inflater.inflate(R.layout.fragment_pointcloud, container, false);
+        mWebview = (WebView) v.findViewById(R.id.webview);
+        mWebview.loadUrl("http://192.168.2.101/potree/pageName.html");
+
+        //Enable Javascript
+        WebSettings webSettings = mWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //Force links and redirects to open in the WebView instead of in a browser
+        mWebview.setWebViewClient(new WebViewClient());
+
+        return v;
     }
 
 }
